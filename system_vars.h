1c67f23798b41a73589cfbbbb108004ac87c93cd
Mohammad Mostafa Dastjerdi <ezmmd83@gmail.com>
2021-01-17 20:41:05 +0330

    created to get system variables

---
+++ system_vars.h
@@ -0,0 +1,39 @@
+using namespace std;
+using namespace chrono;
+
+namespace cli::system_vars {
+	string command_show_date(const string &param) {
+		auto now = system_clock::now();
+		time_t time = system_clock::to_time_t(now);
+		
+		struct tm t_struct;
+		localtime_s(&t_struct, &time);
+		
+		char time_format[50];
+		char time_buffer[100];
+		
+		if (param == "s" || param.empty()) strcpy_s(time_format, sizeof(time_format), "%F");
+		else if (param == "m") strcpy_s(time_format, sizeof(time_format), "%a - %F");
+		else if (param == "l") strcpy_s(time_format, sizeof(time_format), "%a - %b %d, %Y");
+		else if (param == "f") strcpy_s(time_format, sizeof(time_format), "%A - %d of %B, %Y");
+		else return "";
+		
+		strftime(time_buffer, sizeof(time_buffer), time_format, &t_struct);
+		return time_buffer;
+	}
+	
+	string command_show_time(const string &param) {
+		auto now = system_clock::now();
+		time_t time = system_clock::to_time_t(now);
+		
+		struct tm t_struct;
+		localtime_s(&t_struct, &time);
+		
+		if (param == "s" || param.empty()) {
+			char time_buffer[50];
+			strftime(time_buffer, sizeof(time_buffer), "%X", &t_struct);
+			return time_buffer;
+		} else if (param == "l") return "echo";
+		else return "";
+	}
+}
